b4f0d6b43f66c50be6c37a3be996a3d05973d450
tdang021 <tdang021@users.noreply.github.com>
2024-04-19 01:29:27 -0400

    Created basic weather app structure with css styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,58 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Weather App</h1>
-        </header>
-        <button type="button" className="btn btn-primary">
-          Search
-        </button>
+        <div className="app-container" class="border p-3">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city..."
+                  autofocus="on"
+                  class="w-100 h-100"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  class="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+
+          <h1>Paris</h1>
+          <h5>Friday, April 19 15:30</h5>
+
+          <div class="row">
+            <div class="col-6">
+              <h6>Clear sky</h6>
+              <h5>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                  alt="clear sky"
+                />{" "}
+                15°C
+              </h5>
+            </div>
+            <div class="col-6">
+              <h6>Humidity: 10%</h6>
+              <h6>Precipitation: 20%</h6>
+              <h6>Wind: 6 km/h</h6>
+            </div>
+          </div>
+        </div>
+
         <footer>
-          This website was coded by Tiffany and is open-sourced on GitHub
+          This website was coded by Tiffany and is open-sourced on{" "}
+          <a
+            href="https://github.com/tdang021/weather-app-1.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
         </footer>
       </div>
     </div>
